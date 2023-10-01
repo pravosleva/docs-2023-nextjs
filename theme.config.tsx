1bed704fe3d63add68f85eace943cab8b1eb6e23
Den <selection4test@gmail.com>
2023-10-01 12:37:37 +0300

    content updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Docs exp with Next.js</span>,
+  logo: <span>pravosleva.pro</span>,
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
   chat: {
-    link: 'http://pravosleva.ru/express-helper/chat/#/?room=ui-test',
+    link: 'https://pravosleva.pro/express-helper/chat/#/?room=pravosleva.pro',
     icon: () => {
       return (
         <b>CHAT</b>
       )
     },
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
   },
